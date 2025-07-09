68da6a35431fed05afde1ad38fecf6c70f2375a1
Karan Singh <karansinghkachwah@gmail.com>
2025-07-10 04:20:53 +0530

    hide notif in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -622,16 +622,6 @@ const Navbar: React.FC = () => {
               {/* User Section */}
               {user ? (
                 <div className="flex items-center space-x-3">
-                  {/* Notifications */}
-                  <button className={`relative p-2 rounded-lg transition-colors duration-200 ${
-                    isDark 
-                      ? 'text-gray-400 hover:text-gray-300 hover:bg-slate-800' 
-                      : 'text-gray-500 hover:text-gray-700 hover:bg-gray-50'
-                  }`}>
-                    <Bell className="h-5 w-5" />
-                    <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
-                  </button>
-
                   {/* User Menu */}
                   <div className="relative">
                     <button
